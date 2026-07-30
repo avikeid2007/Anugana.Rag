746cab57d985866acdd4ef99bae4eecda3192d66
Avnish Kumar <avnish@ymail.com>
2026-07-30 12:13:21 +0530

    feat: configure Android splash screen styles and implement multi-platform CI/CD release workflow

--- Anugana.Rag/Platforms/Android/MainActivity.Android.cs
+++ Anugana.Rag/Platforms/Android/MainActivity.Android.cs
@@ -17,8 +17,25 @@ public class MainActivity : Microsoft.UI.Xaml.ApplicationActivity
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         global::AndroidX.Core.SplashScreen.SplashScreen.InstallSplashScreen(this);
+        global::AndroidX.Activity.EdgeToEdge.Enable(this);
 
         base.OnCreate(savedInstanceState);
+
+        if (Window != null)
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+            {
+                Window.SetDecorFitsSystemWindows(false);
+            }
+
+            if (Build.VERSION.SdkInt < (BuildVersionCodes)35) // API < 35 (Android 15)
+            {
+#pragma warning disable CS0618
+                Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+                Window.SetNavigationBarColor(Android.Graphics.Color.Transparent);
+#pragma warning restore CS0618
+            }
+        }
     }
 
 }
